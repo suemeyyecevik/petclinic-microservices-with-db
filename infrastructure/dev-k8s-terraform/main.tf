195845cb588ca5e41f4506d563cf81d491f5811f
suemeyyecevik <kardaysumeyye@gmail.com>
2025-09-12 09:04:46 +0000

    subnet-ıd

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -90,7 +90,7 @@ resource "aws_instance" "kube-master" {
   iam_instance_profile = aws_iam_instance_profile.petclinic-master-server-profile.name
   vpc_security_group_ids = [aws_security_group.k8s-sec-gr.id]
   key_name = "clarus"
-  subnet_id = "subnet-0e04c1aede47ab507"  # select own subnet_id of us-east-1a
+  subnet_id = "subnet-02b0916faefe98b21"  # select own subnet_id of us-east-1a
   availability_zone = "us-east-1a"
   tags = {
     Name = "kube-master"
@@ -106,7 +106,7 @@ resource "aws_instance" "worker-1" {
   instance_type = "t3a.medium"
   vpc_security_group_ids = [aws_security_group.k8s-sec-gr.id]
   key_name = "clarus"
-  subnet_id = "subnet-0e04c1aede47ab507"  # select own subnet_id of us-east-1a
+  subnet_id = "subnet-02b0916faefe98b21"  # select own subnet_id of us-east-1a
   availability_zone = "us-east-1a"
   tags = {
     Name = "worker-1"
@@ -122,7 +122,7 @@ resource "aws_instance" "worker-2" {
   instance_type = "t3a.medium"
   vpc_security_group_ids = [aws_security_group.k8s-sec-gr.id]
   key_name = "clarus"
-  subnet_id = "subnet-0e04c1aede47ab507"  # select own subnet_id of us-east-1a 
+  subnet_id = "subnet-02b0916faefe98b21"  # select own subnet_id of us-east-1a 
   availability_zone = "us-east-1a"
   tags = {
     Name = "worker-2"
